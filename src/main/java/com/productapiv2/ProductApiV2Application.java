3997fe825dd65b751edff58e590fad905346f0cd
mrozik52 <pmroz1995@gmail.com>
2020-06-29 22:26:46 +0200

    init project

---
+++ src/main/java/com/productapiv2/ProductApiV2Application.java
@@ -0,0 +1,14 @@
+package com.productapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+ProductApiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/productapiv2/productapiv2/ProductApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productapiv2.productapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
